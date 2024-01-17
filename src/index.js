0206720598ff1c46a119f669c8d6bce03bcd67d4
AyandaMajola01 <Ayandamajola1001@gmail.com>
2024-01-17 18:24:27 +0200

    Added more weather data.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,33 @@ function updateWeather(response) {
    let temperatureElement=document.querySelector("#weather-temperature");
    let temperature=response.data.temperature.current;
    let cityElement=document.querySelector("#city");
-   
+   let descriptionElement=document.querySelector("#description");
+   let humidityElement=document.querySelector("#humidity");
+   let windElement=document.querySelector("#wind");
+   let timeElement=document.querySelector("#time");
+  let date= new Date(response.data.time*1000);
+console.log(response);
    cityElement.innerHTML=response.data.city;
+  timeElement.innerHTML=formatDate(date);
+   descriptionElement.innerHTML=response.data.condition.description;
+   humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+   windElement.innerHTML=`${response.data.wind.speed}km/h`;
    temperatureElement.innerHTML= Math.round(temperature);
 }
+
+function formatDate(date) {
+   let minutes= date.getMinutes();
+   let hours= date.getHours();
+   let days=["Monday","Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+let day=days[date.getDay()];
+   
+if(minutes<10) {
+    minutes= `0${minutes}`
+};
+
+return `${day} ${hours}:${minutes}`;
+}
+
 
 function searchCity(city) {
 let apiKey="d5cf5f78a130388886eo4652tf8e3a0b";
